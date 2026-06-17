4311f5f1bd6d1bb74887593b4f2d9cb53a9d8b46
mikeyoung83 <mikeby83@gmail.com>
2026-06-16 20:52:39 -0400

    adding archived sp pages

--- src/components/ArrowCard.tsx
+++ src/components/ArrowCard.tsx
@@ -16,7 +16,7 @@ export default function ArrowCard({ entry, pill, projectColor }: Props) {
   return (
     <a
       href={`/${entry.collection}/${entry.slug}`}
-      class="group p-4 gap-3 flex items-center border rounded-lg hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-colors duration-300 ease-in-out"
+      class="group p-4 gap-3 flex items-center border-2 rounded-lg hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-colors duration-300 ease-in-out"
       style={borderStyle}
     >
       <div class="w-full group-hover:text-black group-hover:dark:text-white blend">
---
+++ src/content/projects/scalepad/index.md
@@ -0,0 +1,12 @@
+---
+title: "ScalePad"
+summary: "update"
+date: "Sep 05 2025"
+draft: false
+tags:
+- WordPress
+demoUrl: https://www.scalepad.com/
+projectColor: '#112D3C'
+---
+
+You can check out the [Live Legacy Demo](/archive/sp/ignition/index.html) to see the original implementation.
